3560bdffe4568599490693cfcb3fbc8a18b341a9
ibro4470 <yashimthaddeus72@gmail.com>
2025-01-12 07:18:03 -0800

    second push

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
 from tkinter import*
 
 
-windows = Tk()
-windows.geometry("300x300")
-windows.title("dictionaries")
+windows2 = Tk()
+windows2.geometry("300x300")
+windows2.title("IGBO DICTIONARIES")
+entry_text = Entry(windows2)
+entry_text.pack()
+
+result = StringVar()
+result_label = Label(windows2, textvariable=result)
+result_label.pack()
+igbo_dictionary = {
+    "stand": 'guo',
+    "joy": 'onu',
+    "healthy": 'ahu ike',
+    "light": 'ohuru',
+    "sit": 'nodu ala',
+    "black": 'oji',
+    "lies": 'ugha',
+    "sickness": 'noria',
+    "fire": 'oku',
+    "come": 'bia',
+    "child": 'nwa',
+    "man": 'nweke',
+    "cap": 'okpu',
+    "house": 'ulo',
+    "beans": 'agwa',
+    "friend": 'enyi',
+    "festival": 'emume',
+    "crown": 'okpu eze',
+    "clock": 'elekere',
+}
+def check(word):
+    if word in igbo_dictionary:
+        result.set(igbo_dictionary[word])
+        print(igbo_dictionary[word])
+
+    else:
+        result.set("Not found")
+        print("Not found")
+
+
+igbo_button = Button(windows2, text="igbo", command=lambda: check(entry_text.get()))
+igbo_button.pack()
 
 
 
-windows.mainloop()
+
+
+windows2.mainloop()
